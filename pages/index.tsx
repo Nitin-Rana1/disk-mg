dfa60f74c535cd144918401f0a38e2cd9655d7fc
Nitin Rana <nitinranagraphian@gmail.com>
2023-07-22 06:27:39 +0530

    app link

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 export default function Home() {
   // Combine the file URL and access token to create the temporary download link
   const downloadUrl =
-    "https://firebasestorage.googleapis.com/v0/b/vdo-call-9932f.appspot.com/o/DiskMGContextMenuInstaller.exe?alt=media&token=6b5db299-78da-4eec-bfc8-6cfb0b619d90";
+    "https://firebasestorage.googleapis.com/v0/b/vdo-call-9932f.appspot.com/o/Disk_MG.exe?alt=media&token=2967fd90-b41c-4b34-a63e-750d818aa6e3";
 
   return (
     <div className={styles.container}>
